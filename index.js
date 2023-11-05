2cd0d1d061b86b94b194aef54b0e52468a652a62
Guetix <hamzagatoui09@gmail.com>
2023-11-05 17:04:08 +0100

    add functions

--- index.js
+++ index.js
@@ -11,12 +11,29 @@ class Calculator {
         this.resultScreentextContent.innerText = '' || 0
         this.entriesScreentextContent.innerText = ''
     }
-    Delete() {
+    backspace() {
+        let resultValue = this.resultScreentextContent.innerText
+        let currentValue = this.entriesScreentextContent.innerText;
+        if (resultValue != false && currentValue === "") {
+            this.updateEntriersScreen(resultValue.slice(0 , -1))
+            return
+        }
+        
+        if (!this.firstOperand) return
+        if (this.secondOperand) {
+            this.secondOperand = this.secondOperand.slice(0, -1);
+        } else if (this.operation) {
+            this.operation = null;
+        } else {
+            this.firstOperand = this.firstOperand.slice(0, -1);
+        }
 
+       
+        this.entriesScreentextContent.innerText = currentValue.slice(0, -1);
     }
-    point() {
+    appendDecimalPoint() {
+        if (!this.firstOperand) return
         if (!this.operation) {
-            if (this.firstOperand == null) return
             if (this.firstOperand?.includes('.')) return
             this.firstOperand = this.firstOperand + '.'
             this.updateEntriersScreen('.')
@@ -29,21 +46,21 @@ class Calculator {
     }
 
     addOperation(operation) {
-        if (!this.firstOperand) return
-        if (this.resultScreentextContent.innerText != false &&
-            this.entriesScreentextContent.innerText === "") this.updateEntriersScreen(
-                this.resultScreentextContent.innerText
-            )
-        if (operation === ".") {
-            this.point()
-            return
+        if (!this.firstOperand) return;
+        let currentValue = this.entriesScreentextContent.innerText;
+        let resultValue = this.resultScreentextContent.innerText
+
+        // If there's a result and the current value is empty, update the entries screen with the result
+        if (resultValue !== "" && currentValue === "") {
+            this.updateEntriersScreen(resultValue)
         }
         if (this.operation) {
-            if (this.secondOperand) {
+            
+            if (this.secondOperand != false) {
                 this.equal(operation)
                 return
             }
-            this.entriesScreentextContent.innerText = this.entriesScreentextContent.innerText.slice(0, -1)
+            this.entriesScreentextContent.innerText = currentValue.slice(0, -1)
         }
         this.operation = operation
 
@@ -51,7 +68,6 @@ class Calculator {
     }
 
     appendNumber(number) {
-
         if (!this.operation) {
             if (this.resultScreentextContent.innerText != false) return
             this.firstOperand = this.firstOperand.toString() + number
@@ -63,28 +79,31 @@ class Calculator {
 
     equal(opt) {
         if (!this.firstOperand || !this.secondOperand) return
-        const fOpr = parseFloat(this.firstOperand)
-        const sOpr = parseFloat(this.secondOperand)
+        const firstOperand = parseFloat(this.firstOperand)
+        const secondOperand = parseFloat(this.secondOperand)
         let result;
-        if (fOpr && sOpr) {
+        if (firstOperand && secondOperand) {
             switch (this.operation) {
                 case "+":
-                    result = fOpr + sOpr
+                    result = firstOperand + secondOperand
                     break
                 case "−":
-                    result = fOpr - sOpr
+                    result = firstOperand - secondOperand
                     break
                 case "×":
-                    result = fOpr * sOpr
+                    result = firstOperand * secondOperand
                     break
                 case "÷":
-                    result = fOpr / sOpr
+                    result = firstOperand / secondOperand
                     break
                 default: return
             }
             result = result.toFixed(2)
-            this.redefine(result)
             this.updateResultScreen(result)
+            this.firstOperand = result
+            this.secondOperand = ''
+            this.operation = null
+
             if (opt) this.addOperation(opt)
         }
     }
@@ -100,12 +119,6 @@ class Calculator {
         this.resultScreentextContent.classList.add('light-color')
         this.entriesScreentextContent.innerText = ""
     }
-
-    redefine(result) {
-        this.firstOperand = result
-        this.secondOperand = ''
-        this.operation = null
-    }
 }
 
 
@@ -114,6 +127,7 @@ const resultScreen = document.getElementsByClassName('calculation-result')[0]
 const clearAllButton = document.querySelector('[data-clear-all]')
 const deleteButton = document.querySelector('[data-delete]')
 const equalsButton = document.querySelector('[data-equal]')
+const decimalPointButton = document.querySelector('[data-decimal-Point')
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 
@@ -134,6 +148,14 @@ operationButtons.forEach(btn => {
 clearAllButton.addEventListener('click', () => {
     calculator.clearAll()
 })
+deleteButton.addEventListener('click', () => {
+    calculator.backspace()
+})
 equalsButton.addEventListener("click", () => {
     calculator.equal()
 })
+decimalPointButton.addEventListener('click', () => {
+    calculator.appendDecimalPoint()
+})
+
+// __GUETIX
